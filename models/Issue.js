95ea86d02fbe439812d88ccd8519cf702f8e7e72
manik8 <maniksharma2014.ms@gmail.com>
2020-08-24 03:41:09 +0530

    Some Backend Changes

--- models/Issue.js
+++ models/Issue.js
@@ -3,17 +3,44 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostIssue = new Schema({
+  id: {
+    type: String,
+    default: null,
+    required: true,
+  },
   name: {
     type: String,
+    default: null,
     required: true,
   },
   description: {
     type: String,
+    default: null,
     required: true,
   },
-  date: {
-    type: Date,
-    default: Date.now,
+  url: {
+    type: String,
+    default: null,
+    required: true,
+  },
+  number: {
+    type: Number,
+    default: null,
+    required: true,
+  },
+  label: {
+    type: String,
+    default: null,
+  },
+  state: {
+    type: String,
+    default: null,
+    required: true,
+  },
+  locked: {
+    type: Boolean,
+    default: null,
+    required: true,
   },
 });
 
--- routes/api/issues.js
+++ routes/api/issues.js
@@ -10,23 +10,43 @@ const ISSUE = require("../../models/Issue");
 router.post(
   "/add-issue",
   [
+    check("id", "ID is Required").not().isEmpty(),
     check("name", "Project Name is Required").not().isEmpty(),
     check("description", "Issue Description is required").not().isEmpty(),
+    check("url", "Project URL is Required").not().isEmpty(),
+    check("number", "Number is required").not().isEmpty(),
+    check("state", "State is required").not().isEmpty(),
+    check("locked", "Locked is required").isBoolean(),
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).json({ errors: errors.array() });
     }
-    const { name, description } = req.body;
+    const {
+      id,
+      name,
+      description,
+      url,
+      number,
+      label,
+      state,
+      locked,
+    } = req.body;
     try {
       const newIssue = new ISSUE({
+        id,
         name,
         description,
+        url,
+        number,
+        label,
+        state,
+        locked,
       });
 
       const issue = await newIssue.save();
-      res.send(issue);
+      res.status(200).send("1 record inserted successfully");
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
@@ -39,9 +59,16 @@ router.post(
 // @access   Public
 
 router.get("/list-issue", async (req, res) => {
+  const page = req.query.page;
+  const limit = req.query.limit;
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
   try {
-    const issues = await ISSUE.find().sort({ date: -1 });
-    res.send(issues);
+    let issues = await ISSUE.find();
+    issues = issues.slice(startIndex, 10);
+    res.status(200).send("Successfully fetched 10 records");
+    res.json(issues);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
@@ -53,16 +80,22 @@ router.get("/list-issue", async (req, res) => {
 // @access   Public
 
 router.patch("/update-issue/:id", async (req, res) => {
-  const { description } = req.body;
+  const { name, description, url, number, label, state, locked } = req.body;
   try {
     let issue = await ISSUE.findById(req.params.id);
 
     if (issue) {
+      issue.name = name;
       issue.description = description;
-      issue = await issue.save();
+      issue.url = url;
+      issue.number = number;
+      issue.label = label;
+      issue.state = state;
+      issue.locked = locked;
     }
+    await issue.save();
 
-    res.send(issue);
+    res.status(200).send("1 record updated successfully");
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
@@ -78,7 +111,7 @@ router.delete("/delete-issue/:id", async (req, res) => {
     const issue = await ISSUE.findById(req.params.id);
     await issue.remove();
 
-    res.send("Issue has been Deleted");
+    res.send("1 record deleted successfully");
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
